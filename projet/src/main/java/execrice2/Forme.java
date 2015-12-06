b377577d7a700d44bafebe7e65c2958af4cc4a75
Saiboudine <chaiboudine@yahoo.fr>
2015-12-06 16:32:18 +0100

    mise à jour

---
+++ projet/src/main/java/execrice2/Forme.java
@@ -0,0 +1,11 @@
+package execrice2;
+
+import java.awt.Color;
+
+public abstract class Forme {
+	protected Color couleur;
+	
+	/**Fonction permettant d'obtenir la superficie d'une forme fermée*/
+	public abstract float calculerSuperficie();
+
+}
